a07a193bee7a9d77eed8c3c1a867886d6cb8aaa0
Ibrahim Githinji <194280177+Ibrahimgithinji@users.noreply.github.com>
2026-08-29 16:36:26 +0300

    Wire Sweatshirts section into the home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import Hero from "@/components/Hero";
 import FeaturedProducts from "@/components/FeaturedProducts";
+import SweatshirtsSection from "@/components/SweatshirtsSection";
 import Testimonials from "@/components/Testimonials";
 import Newsletter from "@/components/Newsletter";
 export default function Home() {
@@ -7,6 +8,7 @@ export default function Home() {
     <>
       <Hero />
       <FeaturedProducts />
+      <SweatshirtsSection />
       <Testimonials />
       <Newsletter />
     </>
